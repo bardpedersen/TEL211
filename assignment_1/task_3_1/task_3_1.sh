5d3314ac33bf6a2217127e6c49f647ed33685244
Bård <bardped21@gmail.com>
2023-02-10 18:30:42 +0100

    Done with task 2, started on task 3

--- assignment_1/task_3_1/task_3_1.sh
+++ assignment_1/task_3_1/task_3_1.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 #Task 1
+
+read -p "Wich file with (directory) do you want to read" dir
+
+if [ $# != 1 ]; then
+    echo "You need to pass in one argument, no more or less."
+    exit
+fi
+
+
 characters=3
 words=0
 lines=0
